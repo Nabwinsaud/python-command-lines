4c1d5240e92aced251e6a0128650c6789125e1af
Nabwinsaud <nabinsaud2059@gmail.com>
2021-05-11 12:11:37 +0545

    python pyaudio fix

---
+++ file.py
@@ -0,0 +1,11 @@
+# installing pyaudio iin window....
+ 
+# pip install pyAudio/audio.... doesnt work in all system
+
+
+#what to do then....
+# steps:
+'''
+step1.pip install pipwin
+step2.pipwin install pyaudio
+'''
